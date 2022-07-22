382f26b5b7b2dee55804033f09409784231aaa44
VLAS <https://github.com/VLAS-OVERONE>
2022-07-22 18:32:40 +0300

    first commit

---
+++ My first project.py
@@ -0,0 +1,18 @@
+# По введённому имени с помощью функции определяем пол человека
+
+dict = {
+    'Andrey': 'male',
+    'Sergey': 'male',
+    'Alexandr': 'male',
+    'Sveta': 'female',
+    'Tanya': 'female',
+    'Katya': 'female',
+}
+
+def sex(key, dict):
+    for k, v in dict.items():
+        if k == key: return v
+    return "There is no such name in the list"
+
+print(sex(input("Введите Имя: \n"), dict))
+
